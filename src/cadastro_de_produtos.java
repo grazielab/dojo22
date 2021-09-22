b319a46e5a9b4e8282aa9acd2e88fec0945a0b62
Jéssica Argentino <jessicaargentino@hotmail.com>
2021-09-22 17:43:33 -0300

    Cabeçalho iniciado

--- src/cadastro_de_produtos.java
+++ src/cadastro_de_produtos.java
@@ -1,5 +1,15 @@
 public class cadastro_de_produtos {
     public static void main(String[] args) {
+        cabecalho();
+        imprimeMenu();
+    }
 
+    static void cabecalho() {
+        System.out.println("***** Cadastro de produtos *****");
+    }
+    static void imprimeMenu(){
+        System.out.println("\t1 - Cadastrar produto");
+        System.out.println("\t2 - Cadastrar categoria");
+        System.out.println("\t0 - Sair");
     }
 }
