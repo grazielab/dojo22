6387dfb33b8fcd4cadab0136f44aae0c9410c60a
daniela150salvador@gmail.com <daniela.salvador@capgemini.com>
2021-09-22 17:58:51 -0300

    dojo Ale e Dani

--- src/cadastro_de_produtos.java
+++ src/cadastro_de_produtos.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class cadastro_de_produtos {
+
+    static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         cabecalho();
         imprimeMenu();
+        menu();
     }
 
     static void cabecalho() {
@@ -11,5 +17,21 @@ public class cadastro_de_produtos {
         System.out.println("\t1 - Cadastrar produto");
         System.out.println("\t2 - Cadastrar categoria");
         System.out.println("\t0 - Sair");
+    }
+    static void menu (){
+        int opcao = 0;
+        System.out.println("Digite a sua opção: ");
+        opcao = scan.nextInt();
+        switch (opcao){
+            case 1:
+                System.out.println("Cadastrar produto: ");
+                break;
+            case 2:
+                System.out.println("Cadastrar categoria: ");
+                break;
+            case 0:
+                System.out.println("Programa encerrado!");
+                break;
+        }
     }
 }
